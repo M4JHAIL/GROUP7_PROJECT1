43ebc7b805dbb576f9f9744444fccf568319506b
M4JHAIL <gurkaran@sheridancollege.ca>
2024-11-30 23:07:42 -0500

    Delete src directory

--- src/ca/sheridancollege/project/SnakeGame.java
+++
@@ -1,9 +0,0 @@
-package ca.sheridancollege.project;
-
-public class SnakeGame {
-    public static void main(String[] args) {
-        SnakesAndLaddersGame game = new SnakesAndLaddersGame("Snakes and Ladders");
-        game.addPlayers();
-        game.play();
-    }
-}
